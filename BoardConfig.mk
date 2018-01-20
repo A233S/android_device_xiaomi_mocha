fcf738a0b53a7a8bf4b73729edfb02e51dd76c24
Tommy Wu <wu.tommy@gmail.com>
2018-01-20 21:45:34 +0800

    TWRP sheild

---
+++ BoardConfig.mk
@@ -0,0 +1,97 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
+
+# Architecture
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a15
+# Board
+TARGET_BOARD_PLATFORM := tegra
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+# Use unified vendor
+TARGET_TEGRA_VARIANT := shield
+
+# Kernel
+BOARD_KERNEL_CMDLINE := vpr_resize androidboot.selinux=permissive androidboot.hardware=tn8
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_RAMDISK_OFFSET := 0x02000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) 
+BOARD_CUSTOM_BOOTIMG_MK := device/xiaomi/mocha/mkbootimg.mk
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 671088640 # 640 Mb stock partition table
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1337564160 # 1.2 Gb
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13742637056
+BOARD_CACHEIMAGE_PARTITION_SIZE := 402653184
+BOARD_BOOTIMAGE_PARTITION_SIZE := 20971520
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20971520
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# PowerHAL
+TARGET_POWERHAL_VARIANT := tegra
+
+# ThermalHAL
+TARGET_THERMALHAL_VARIANT := tegra
+
+# Graphics
+USE_OPENGL_RENDERER := true
+BOARD_DISABLE_TRIPLE_BUFFERED_DISPLAY_SURFACES := true
+
+# Offmode Charging
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BACKLIGHT_PATH := "/sys/class/backlight/lcd-backlight/brightness"
+RED_LED_PATH := "/sys/class/leds/red/brightness"
+GREEN_LED_PATH := "/sys/class/leds/green/brightness"
+BLUE_LED_PATH := "/sys/class/leds/blue/brightness"
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# Per-application sizes for shader cache
+MAX_EGL_CACHE_SIZE := 4194304
+MAX_EGL_CACHE_ENTRY_SIZE := 262144
+
+# Recovery
+TARGET_PREBUILT_KERNEL := device/xiaomi/mocha/kernel
+TARGET_RECOVERY_DEVICE_DIRS += device/xiaomi/mocha
+TARGET_RECOVERY_FSTAB := device/xiaomi/mocha/twrp.fstab
+BOARD_NO_SECURE_DISCARD := true
+
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# TWRP Configuration
+TW_THEME := portrait_hdpi
+TW_INCLUDE_FUSE_EXFAT := true
+TW_INCLUDE_NTFS_3G := true
+TW_EXCLUDE_SUPERSU := true
+TWRP_INCLUDE_LOGCAT := true
+TW_EXTRA_LANGUAGES := true
+
+PRODUCT_COPY_FILES += device/xiaomi/mocha/dt.img:dt.img
