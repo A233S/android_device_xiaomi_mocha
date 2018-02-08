51377ae6b675cca9756bd4fbdd260750aa34175f
Tommy Wu <wu.tommy@gmail.com>
2018-02-08 22:09:53 +0800

    change for dotOS

--- dot.mk
+++ dot.mk
@@ -1,10 +1,10 @@
 # Inherit device configuration for mocha.
 $(call inherit-product, device/xiaomi/mocha/full_mocha.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_tablet_wifionly.mk)
+# Inherit some common dotOS stuff.
+$(call inherit-product, vendor/dot/config/common_mini_tablet_wifionly.mk)
 
-PRODUCT_NAME := lineage_mocha
+PRODUCT_NAME := dot_mocha
 PRODUCT_DEVICE := mocha
 BOARD_VENDOR := Xiaomi
 
