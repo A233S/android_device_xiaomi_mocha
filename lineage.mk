606a7d40e7f9a4a4689c36ca87cde0ef3441a685
Tommy Wu <wu.tommy@gmail.com>
2018-02-11 09:33:18 +0800

    include time for release date

--- lineage.mk
+++ lineage.mk
@@ -7,5 +7,6 @@ $(call inherit-product, vendor/cm/config/common_mini_tablet_wifionly.mk)
 PRODUCT_NAME := lineage_mocha
 PRODUCT_DEVICE := mocha
 BOARD_VENDOR := Xiaomi
+LINEAGE_VERSION_APPEND_TIME_OF_DAY := TRUE
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
