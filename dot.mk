fc3312795f0020d560a83519c1a3918b71793bb8
Tommy Wu <wu.tommy@gmail.com>
2018-03-27 22:19:01 +0800

    dotOS using common.mk

--- dot.mk
+++ dot.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/xiaomi/mocha/full_mocha.mk)
 
 # Inherit some common dotOS stuff.
-$(call inherit-product, vendor/dot/config/common_mini_tablet_wifionly.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 PRODUCT_NAME := dot_mocha
 PRODUCT_DEVICE := mocha
