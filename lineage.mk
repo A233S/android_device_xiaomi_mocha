04311d0ce3a8fb7f5e63da805ecc8887910928fd
Tommy Wu <wu.tommy@gmail.com>
2018-05-23 09:53:38 +0800

    change for crDroid

--- lineage.mk
+++ lineage.mk
@@ -2,9 +2,9 @@
 $(call inherit-product, device/xiaomi/mocha/full_mocha.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/rr/config/common_mini_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_mini_tablet_wifionly.mk)
 
-PRODUCT_NAME := rr_mocha
+PRODUCT_NAME := lineage_mocha
 PRODUCT_DEVICE := mocha
 BOARD_VENDOR := Xiaomi
 LINEAGE_VERSION_APPEND_TIME_OF_DAY := TRUE
