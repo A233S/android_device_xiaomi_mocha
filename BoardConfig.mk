f0c60695bf908b2eadfcc86eabb39095a87532e0
arttttt <artem-bambalov@yandex.ru>
2018-06-30 21:42:43 +0300

    mocha: set heap props for 2Gb-3Gb dynamically
    Signed-off-by: arttttt <artem-bambalov@yandex.ru>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,10 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_EXFAT_DRIVER := sdfat
 
+# Init
+TARGET_INIT_VENDOR_LIB := mocha_init
+TARGET_RECOVERY_DEVICE_MODULES := mocha_init
+
 # Kernel
 BOARD_KERNEL_CMDLINE := vpr_resize androidboot.selinux=permissive androidboot.hardware=tn8
 BOARD_KERNEL_BASE := 0x10000000
