c56bf898ad0569f5c8244e64fa3fdb0f089c3116
Tommy Wu <wu.tommy@gmail.com>
2018-11-08 22:00:09 +0800

    modified for aex

--- ChargerTile/Android.mk
+++ ChargerTile/Android.mk
@@ -5,8 +5,8 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
     $(call all-java-files-under, src)
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
+#LOCAL_STATIC_JAVA_LIBRARIES := \
+#    org.lineageos.platform.internal
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res
